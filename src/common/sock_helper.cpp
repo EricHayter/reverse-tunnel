4f53dee8773595cf9f1031f43de8def882a98f32
Eric <hayter.eric@gmail.com>
2026-07-04 10:52:22 -0400

    Fix bug on read bytes side not ending loop. Removed logging

--- src/common/sock_helper.cpp
+++ src/common/sock_helper.cpp
@@ -4,32 +4,28 @@
 #include <sys/socket.h>
 #include <errno.h>
 
-#include <iostream>
-#include <format>
-
 bool read_bytes(int socket_fd, std::span<std::byte> buffer)
 {
     while (true) {
         int recv_size = recv(socket_fd, buffer.data(), buffer.size_bytes(), 0x00);
         // socket got closed
-        if (recv_size == 0) return false;
+        if (recv_size == 0) return 0;
         // error of some sort
         if (recv_size < 0) {
             // the read was interrupted for whatever reason
             if (errno == EINTR) continue;
-            return false;
+            return errno;
         }
         buffer = buffer.subspan(recv_size);
+        if (buffer.empty())
+            return {};
     }
-    return true;
 }
 
 std::optional<int> send_bytes(int socket_fd, std::span<const std::byte> buffer)
 {
-    std::cout << std::format("starting to send {} bytes\n", buffer.size_bytes());
     while (true) {
         int send_size = send(socket_fd, buffer.data(), buffer.size_bytes(), 0x00);
-        std::cout << std::format("sent {} bytes\n", send_size);
         // socket got closed
         if (send_size == 0) return 0;
         // error of some sort
