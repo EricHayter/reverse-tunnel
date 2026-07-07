91d4f97b18506c1bd9fb6708ba0d55003db7e2fd
Eric <hayter.eric@gmail.com>
2026-07-07 06:49:29 -0400

    Trying to use interruptable blocking with wait using stop token

--- include/common/locking_queue.h
+++ include/common/locking_queue.h
@@ -7,13 +7,30 @@
 template<typename T>
 class Queue {
 public:
-   void push(T val);
-   T pop();
+    Queue() = default;
+    ~Queue();
+
+    Queue& operator=(const Queue&) = delete;
+    Queue(const Queue&) = delete;
+
+    void push(T val);
+
+    /* Blocks until an element is ready to be popped from the queue. In the
+     * case that the worker thread must be stopped (i.e. stop_requested) we
+     * take a stop_token such that the blocking will be interrupted and early
+     * return a std::nullopt */
+    std::optional<T> pop(std::stop_token st);
 private:
-   std::mutex mut_m;
-   std::condition_variable cond_m;
-   std::queue<T> queue_m;
+    std::mutex mut_m;
+    std::condition_variable_any cond_m;
+    std::queue<T> queue_m;
 };
+
+template<typename T>
+Queue<T>::~Queue()
+{
+    cond_m.notify_all();
+}
 
 template<typename T>
 void Queue<T>::push(T val)
@@ -25,12 +42,12 @@ void Queue<T>::push(T val)
 
 
 template<typename T>
-T Queue<T>::pop()
+std::optional<T> Queue<T>::pop(std::stop_token st)
 {
     std::unique_lock<std::mutex> lk{ mut_m };
-    cond_m.wait(lk, [this](){
-        return !queue_m.empty();
-    });
+    cond_m.wait(lk, st, [this](){ return !queue_m.empty(); });
+    if (st.stop_requested())
+        return std::nullopt;
     T ret = std::move(queue_m.front());
     queue_m.pop();
     return ret;
