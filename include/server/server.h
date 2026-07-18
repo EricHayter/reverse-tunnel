b119ae0b0d42c61dea236dc3896078b44895b672
Eric <hayter.eric@gmail.com>
2026-07-18 10:00:16 -0400

    Improve naming conventions in project

--- include/server/server.h
+++ include/server/server.h
@@ -15,41 +15,41 @@ class Server {
 public:
     explicit Server(std::size_t num_workers);
 private:
-    /* helper function to determine if a socket is a passive socket (i.e.
-     * listen has been called on it */
-    static bool is_listening_socket(int file_descriptor);
-
     /* Handles all of the read events on sockets monitored by the socket
      * monitor */
-    void handle_read_event(int socket_fd);
+    void handle_read_event(int socket);
 
     /* Handles a read event on a passive socket (i.e. an connection request)
      * subscribes the new connection to EPOLL_IN events */
-    std::expected<int, Error> accept_connection(int listener_socket_fd);
+    std::expected<int, Error> accept_connection(int listening_socket);
 
     /* Handles the initial message from the client indicating the desired
      * mappings, and creates listening sockets for ingress packets with
      * create_listening_socket */
-    Error handle_client_init(int socket_fd);
+    Error handle_client_init(int socket);
 
     /* Creates a socket add calls listen on it. Also subscribes for read EPOLLIN
      * events on the socket monitor */
     std::expected<int, Error> create_listening_socket(PortNum port_num);
 
 
+    /**/
+    int client_listener_socket_m;
+    std::unordered_set<int> listening_sockets_m;
+
     /* IMPORTANT!
      * socket monitor must be declared before the worker functions since they
      * pull form the monitor directly */
-    SocketMonitor sock_monitor_m;
+    SocketMonitor socket_monitor_m;
 
     // worker pool stuff
     std::vector<std::jthread> workers_m;
     void worker_func(const std::stop_token& stop_token);
 
     // special case when handling here (since this is where our management occurs I guess)
-    int client_listener_sock_fd_m; // TODO might not need this
     std::unordered_set<int> client_socks_fd_m; // this should realistically be a vector
 
     // stores from-to port mappings
     std::unordered_map<int, int> port_map_m;
+
 };
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -26,14 +26,14 @@ Server::Server(std::size_t num_workers)
     if (!listener_socket_expected) {
         throw std::runtime_error(listener_socket_expected.error().context);
     }
-    client_listener_sock_fd_m = *listener_socket_expected;
+    client_listener_socket_m = *listener_socket_expected;
 }
 
 
 std::expected<int, Error> Server::create_listening_socket(PortNum port_num)
 {
-    int listening_socket_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0x00);
-    if (listening_socket_fd == -1) {
+    int listening_socket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0x00);
+    if (listening_socket == -1) {
         return std::unexpected<Error>{{ .context = "Failed to create a receiving socket" }};
     }
 
@@ -49,18 +49,21 @@ std::expected<int, Error> Server::create_listening_socket(PortNum port_num)
         return std::unexpected<Error>{{ .context = "Failed to get addrinfo for this computer" }};
     }
 
-    errc = bind(listening_socket_fd, local_addr->ai_addr, local_addr->ai_addrlen);
+    errc = bind(listening_socket, local_addr->ai_addr, local_addr->ai_addrlen);
     if (errc == -1) {
         return std::unexpected<Error>{{ .context = "Failed to bind to address" }};
     }
 
     constexpr int BACKLOG_COUNT = 20;
-    errc = listen(listening_socket_fd, BACKLOG_COUNT);
+    errc = listen(listening_socket, BACKLOG_COUNT);
     if (errc == -1) {
         return std::unexpected<Error>{{ .context = "Call to listen() failed" }};
     }
 
-    auto listener_subscribe_failure = sock_monitor_m.subscribe(SocketMonitor::create_listener_event(listening_socket_fd));
+    listening_sockets_m.insert(listening_socket);
+
+    // is this not out of order???? with the stuff above?
+    auto listener_subscribe_failure = sock_monitor_m.subscribe(SocketMonitor::create_listener_event(listening_socket));
     if (listener_subscribe_failure) {
         return std::unexpected(*listener_subscribe_failure);
     }
@@ -70,10 +73,11 @@ std::expected<int, Error> Server::create_listening_socket(PortNum port_num)
     spdlog::info("Listening for external connections on {}:{} (socket fd {})",
             get_addr_string(local_in),
             port_num,
-            listening_socket_fd
+            listening_socket
             );
 
-    return listening_socket_fd;
+
+    return listening_socket;
 }
 
 
@@ -83,19 +87,19 @@ void Server::worker_func(const std::stop_token& stop_token)
     while (!stop_token.stop_requested()) {
         auto event = sock_monitor_m.pull_event(stop_token);
         if (event) {
-            int active_fd = event->data.fd;
+            int notified_socket = event->data.fd;
             switch (event->events) {
             case EPOLLIN: {
-                spdlog::debug("Handling read event on fd {}", active_fd);
+                spdlog::debug("Handling read event on fd {}", notified_socket);
                 handle_read_event(event->data.fd);
                 break;
             }
             case EPOLLHUP: {
-                spdlog::debug("Handling hang-up event on fd {}", active_fd);
+                spdlog::debug("Handling hang-up event on fd {}", notified_socket);
                 break;
             }
             case EPOLLERR: {
-                spdlog::debug("Handling error event on fd {}", active_fd);
+                spdlog::debug("Handling error event on fd {}", notified_socket);
                 break;
             }
             default: {
@@ -107,34 +111,26 @@ void Server::worker_func(const std::stop_token& stop_token)
     }
 }
 
-bool Server::is_listening_socket(int file_descriptor)
-{
-    int val{ };
-    socklen_t len{ sizeof(val) };
-    assert(getsockopt(file_descriptor, SOL_SOCKET, SO_ACCEPTCONN, &val, &len) != -1);
-    return val != 0;
-}
-
-void Server::handle_read_event(int socket_fd)
+void Server::handle_read_event(int socket)
 {
     /* receiving message from clients (message mapping)  */
-    if (client_socks_fd_m.contains(socket_fd)) {
-        handle_client_init(socket_fd);
+    if (client_socks_fd_m.contains(socket)) {
+        handle_client_init(socket);
         return;
     }
 
     /* new connection connection events */
-    if (is_listening_socket(socket_fd)) {
-        auto connection_result = accept_connection(socket_fd);
+    if (listening_sockets_m.contains(socket)) {
+        auto connection_result = accept_connection(socket);
         if (!connection_result) {
-            spdlog::error("Connection acceptance failed on socket fd {}", socket_fd);
+            spdlog::error("Connection acceptance failed on socket fd {}", socket);
             return;
         }
 
         /* special case: this is a client connection, we should make note of
          * these so we can expect a handshake message, see case above */
-        if (socket_fd == client_listener_sock_fd_m) {
-               client_socks_fd_m.insert(socket_fd);
+        if (socket == client_listener_socket_m) {
+               client_socks_fd_m.insert(socket);
         }
         return;
     }
@@ -144,22 +140,22 @@ void Server::handle_read_event(int socket_fd)
     constexpr int MSG_BUFFER_SIZE = 4096;
     std::array<std::byte, MSG_BUFFER_SIZE> msg_buffer{};
     ssize_t num_bytes{};
-    std::span<std::byte> data_span = std::span(msg_buffer).subspan(sizeof(socket_fd) + sizeof(num_bytes));
-    num_bytes = recv(socket_fd, data_span.data(), data_span.size_bytes(), 0x00);
+    std::span<std::byte> data_span = std::span(msg_buffer).subspan(sizeof(socket) + sizeof(num_bytes));
+    num_bytes = recv(socket, data_span.data(), data_span.size_bytes(), 0x00);
     if (num_bytes == -1) {
         // TODO this should return an error I think?
         spdlog::error("recv call failed: {}", strerror(errno)); // NOLINT(concurrency-mt-unsafe)
     }
     assert(num_bytes != -1); // TODO this can fail for non assert types of stuff
 
-    std::memcpy(msg_buffer.data(), &socket_fd, sizeof(socket_fd));
-    std::memcpy(std::span(msg_buffer).subspan(sizeof(socket_fd)).data(), &num_bytes, sizeof(num_bytes));
+    std::memcpy(msg_buffer.data(), &socket, sizeof(socket));
+    std::memcpy(std::span(msg_buffer).subspan(sizeof(socket)).data(), &num_bytes, sizeof(num_bytes));
 
     // TODO can't have this hardcoded to the client sock when we have multiple clients (it'll need the bijective mapping later)
     // send_bytes(client_sock_fd_m, std::span(msg_buffer).subspan(0, sizeof(socket_fd) + sizeof(num_bytes) + num_bytes));
 }
 
-Error Server::handle_client_init(int socket_fd)
+Error Server::handle_client_init(int socket)
 {
     /* handle the mapping message
      *
@@ -169,7 +165,7 @@ Error Server::handle_client_init(int socket_fd)
     std::array<std::byte, 2 * sizeof(PortNum)> buffer{};
 
     uint16_t num_mappings{ 0 };
-    auto read_errc = read_bytes(socket_fd, std::span{ buffer.data(), sizeof(PortNum) } );
+    auto read_errc = read_bytes(socket, std::span{ buffer.data(), sizeof(PortNum) } );
     if (read_errc) {
         return Error{ .context = "Failed to read number of mappings" };
     }
@@ -179,7 +175,7 @@ Error Server::handle_client_init(int socket_fd)
 
     uint16_t mappings_processed{ 0 };
     while (mappings_processed < num_mappings) {
-        read_errc = read_bytes(socket_fd, std::span{ buffer.data(), 2 * sizeof(uint16_t) });
+        read_errc = read_bytes(socket, std::span{ buffer.data(), 2 * sizeof(uint16_t) });
         if (read_errc) {
             return Error{ .context = "Failed to read number of mappings" };
         }
@@ -202,22 +198,22 @@ Error Server::handle_client_init(int socket_fd)
     return {};
 }
 
-std::expected<int, Error> Server::accept_connection(int listener_socket_fd)
+std::expected<int, Error> Server::accept_connection(int listening_socket)
 {
     sockaddr_storage addr{};
     unsigned int addr_len{ sizeof(addr) };
 
-    int new_conn_socket_fd = accept(listener_socket_fd, reinterpret_cast<sockaddr*>(&addr), &addr_len); // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
-    if (new_conn_socket_fd == -1) {
+    int new_socket = accept(listening_socket, reinterpret_cast<sockaddr*>(&addr), &addr_len); // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+    if (new_socket == -1) {
         return std::unexpected{Error{ .context = "Call to accept() failed" }};
     }
 
     spdlog::debug("Accepted connection from {} (socket fd {})",
             get_addr_string(*reinterpret_cast<sockaddr_in*>(&addr)), // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
-            new_conn_socket_fd
+            new_socket
             );
 
     // subscribe it and save it
-    sock_monitor_m.subscribe(SocketMonitor::create_listener_event(new_conn_socket_fd));
-    return new_conn_socket_fd;
+    sock_monitor_m.subscribe(SocketMonitor::create_listener_event(new_socket));
+    return new_socket;
 }
